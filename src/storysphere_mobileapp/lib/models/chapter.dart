a6e605bd78d496523fa6b220f354c55be2c315b0
Nguyễn Quỳnh Như <ngquynhnhu1901@gmail.com>
2024-06-23 11:22:44 +0700

    add story,chapter,comt model

---
+++ src/storysphere_mobileapp/lib/models/chapter.dart
@@ -0,0 +1,86 @@
+
+
+import 'dart:convert';
+import 'dart:core';
+
+List<Chapter> chaptersFromJson(String str) => List<Chapter>.from(json.decode(str).map((x) => Chapter.fromJson(x)));
+
+Chapter chapterFromJson(String str) => Chapter.fromJson(json.decode(str));
+
+String chapterToJson(Chapter data) => json.encode(data.toJson());
+
+class Chapter {
+  Chapter({
+    this.chapterId,
+    this.fkStoryId,
+    this.chapterName,
+    this.chapterUrlKey,
+    this.chapterContent,
+    this.chapterOrder,
+    this.chapterStatus,
+    this.matureContent,
+    this.viewCount,
+    this.voteCount,
+    this.commentCount,
+    this.wordsCount,
+    this.createdDate,
+    this.lastUpdated,
+    this.commercialActivated,
+    this.chapterSellPrice,
+  });
+
+  int? chapterId;
+  int? fkStoryId;
+  String? chapterName;
+  String? chapterUrlKey;
+  String? chapterContent;
+  int? chapterOrder;
+  int? chapterStatus;
+  bool? matureContent;
+  int? viewCount;
+  int? voteCount;
+  int? commentCount;
+  int? wordsCount;
+  DateTime? createdDate;
+  DateTime? lastUpdated;
+  bool? commercialActivated;
+  double? chapterSellPrice;
+
+  factory Chapter.fromJson(Map<String, dynamic> json) => Chapter(
+        chapterId: json["chapterId"],
+        fkStoryId: json["fk_storyId"],
+        chapterName: json["chapterName"],
+        chapterUrlKey: json["chapterUrlKey"],
+        chapterContent: json["chapterContent"],
+        chapterOrder: json["chapterOrder"],
+        chapterStatus: json["chapterStatus"],
+        matureContent: json["maturedContent"],
+        viewCount: json["viewCount"],
+        voteCount: json["voteCount"],
+        commentCount: json["commentCount"],
+        wordsCount: json["wordsCount"],
+        createdDate: DateTime.parse(json["createdDate"]),
+        lastUpdated: DateTime.parse(json["lastUpdate"]),
+        commercialActivated: json["commercialActivated"],
+        chapterSellPrice: json["chapterSellPrice"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "chapterId": chapterId,
+        "fk_storyId": fkStoryId,
+        "chapterName": chapterName,
+        "chapterUrlKey": chapterUrlKey,
+        "chapterContent": chapterContent,
+        "chapterOrder": chapterOrder,
+        "chapterStatus": chapterStatus,
+        "maturedContent": matureContent,
+        "viewCount": viewCount,
+        "voteCount": voteCount,
+        "commentCount": commentCount,
+        "wordsCount": wordsCount,
+        "createdDate": createdDate!.toIso8601String(),
+        "lastUpdate": lastUpdated!.toIso8601String(),
+        "commercialActivated": commercialActivated,
+        "chapterSellPrice": chapterSellPrice,
+      };
+}
---
+++ src/storysphere_mobileapp/lib/models/story.dart
@@ -0,0 +1,114 @@
+
+
+import 'dart:convert';
+import 'dart:core';
+
+List<Story> storiesFromJson(String str) => List<Story>.from(json.decode(str).map((x) => Story.fromJson(x)));
+
+Story storyFromJson(String str) => Story.fromJson(json.decode(str));
+
+String storyToJson(Story data) => json.encode(data.toJson());
+
+class Story {
+  Story({
+    this.storyId,
+    this.storyName,
+    this.storyUrl,
+    this.storyCover,
+    this.storyContentOutline, 
+    this.fkPublisherAccount, 
+    this.bookAuthorName, 
+    this.bookPublisherName, 
+    this.bookISBNcode, 
+    this.bookPublishDate, 
+    this.categoriesAndTags, 
+    this.selfComposedStory, 
+    this.matureContent, 
+    this.published, 
+    this.chapterCount, 
+    this.viewCount, 
+    this.voteCount, 
+    this.commentCount, 
+    this.ratingPoint, 
+    this.createdDate, 
+    this.lastUpdate, 
+    this.commercialActivated, 
+    this.storySellPrice,
+  });
+
+  int? storyId;
+  String? storyName;
+  String? storyUrl;
+  String? storyCover;
+  String? storyContentOutline;
+  int? fkPublisherAccount;
+  String? bookAuthorName;
+  String? bookPublisherName;
+  String? bookISBNcode;
+  DateTime? bookPublishDate;
+  String? categoriesAndTags;
+  bool? selfComposedStory;
+  bool? matureContent;
+  bool? published;
+  int? viewCount;
+  int? voteCount;
+  int? commentCount;
+  int? chapterCount;
+  double? ratingPoint;
+  DateTime? createdDate;
+  DateTime? lastUpdate;
+  bool? commercialActivated;
+  double? storySellPrice;
+
+  factory Story.fromJson(Map<String, dynamic> json) => Story(
+        storyId: json["storyId"],
+        storyName: json["storyName"],
+        storyUrl: json["storyUrl"],
+        storyCover: json["storyCover"],
+        storyContentOutline: json["storyContentOutline"],
+        fkPublisherAccount: json["fk_publisherAccount"],
+        bookAuthorName: json["book_authorName"],
+        bookPublisherName: json["book_publisherName"],
+        bookISBNcode: json["book_ISBNcode"],
+        bookPublishDate: DateTime.parse(json["book_publishDate"]),
+        categoriesAndTags: json["categoriesAndTags"],
+        selfComposedStory: json["selfComposedStory"],
+        matureContent: json["matureContent"],
+        published: json["published"],
+        viewCount: json["viewCount"],
+        voteCount: json["voteCount"],
+        commentCount: json["commentCount"],
+        ratingPoint: json["ratingPoint"],
+        chapterCount: json["chapterCount"],
+        createdDate: DateTime.parse(json["createdDate"]),
+        lastUpdate: DateTime.parse(json["lastUpdate"]),
+        commercialActivated: json["commercialActivated"],
+        storySellPrice: json["storySellPrice"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "storyId": storyId,
+        "storyName": storyName,
+        "storyUrl": storyUrl,
+        "storyCover": storyCover,
+        "storyContentOutline": storyContentOutline,
+        "fk_publisherAccount": fkPublisherAccount,
+        "book_authorName": bookAuthorName,
+        "book_publisherName": bookPublisherName,
+        "book_ISBNcode": bookISBNcode,
+        "book_publishDate": bookPublishDate!.toIso8601String(),
+        "categoriesAndTags": categoriesAndTags,
+        "selfComposedStory": selfComposedStory,
+        "matureContent": matureContent,
+        "published": published,
+        "viewCount": viewCount,
+        "chapterCount": chapterCount,
+        "voteCount": voteCount,
+        "commentCount": commentCount,
+        "ratingPoint": ratingPoint,
+        "createdDate": createdDate!.toIso8601String(),
+        "lastUpdate": lastUpdate!.toIso8601String(),
+        "commercialActivated": commercialActivated,
+        "storySellPrice": storySellPrice,
+      };
+}

00078b893b0e2b1876298a0ab2c892978825ea5e
Nguyễn Quỳnh Như <ngquynhnhu1901@gmail.com>
2024-07-24 19:50:25 +0700

    fix tag UI

--- src/storysphere_mobileapp/lib/views/mywork/add_story_page.dart
+++ src/storysphere_mobileapp/lib/views/mywork/add_story_page.dart
@@ -7,6 +7,8 @@ import 'package:storysphere_mobileapp/constants/string.dart';
 import 'package:storysphere_mobileapp/constants/utils/color_constant.dart';
 import 'package:storysphere_mobileapp/constants/utils/font_constant.dart';
 import 'package:storysphere_mobileapp/constants/utils/icon_svg.dart';
+import 'package:storysphere_mobileapp/models/category.dart';
+import 'package:storysphere_mobileapp/views/main_widgets/bottom_navigator.dart';
 
 @RoutePage()
 class AddStoryPage extends StatefulWidget {
@@ -24,6 +26,24 @@ class _AddStoryPage extends State<AddStoryPage> {
   final TextEditingController storyNameController = TextEditingController();
   final TextEditingController storyContentOutlineController = TextEditingController();
   bool isNonFiction = false;
+  bool isCategorySelectExpand = false;
+  late Widget categorySelect;
+  late Widget tagsSelect;
+  String? selectedItem;
+  List<int> selectedTags = [];
+  List<Category> listTag = [
+    Category(categoryId: 1, categoryName: 'Kinh dị'),
+    Category(categoryId: 2, categoryName: 'Trinh thám'),
+    Category(categoryId: 3, categoryName: 'Cơ khí'),
+    Category(categoryId: 4, categoryName: 'Lãng mạn'),
+  ];
+
+  final List<String> dropdownBookCategories = [
+    'Sách tâm lý', 'Sách kỹ năng', 'Sách y học', 'Sách lịch sử', 'Sách tài chính', 'Sách khoa học công nghệ', 'Sách tôn giáo - tâm linh'
+  ];
+  final List<String> dropdownNovelCategories = [
+    'Bí ẩn','Kinh dị', 'Khoan học viễn tưởng', 'Lãng mạn', 'Cổ tích', 'Truyện thiếu nhi','Viễn tưởng'
+  ];
   
   @override
   void dispose() {
@@ -37,6 +57,7 @@ class _AddStoryPage extends State<AddStoryPage> {
   Widget build(BuildContext context) {
   
     return Scaffold(
+      bottomNavigationBar: const SPBottomNavigationBar(selectedIndex: 2),
       body: SingleChildScrollView(
         child: 
         Padding( padding: EdgeInsets.symmetric(horizontal: 20.sp),
@@ -205,12 +226,125 @@ class _AddStoryPage extends State<AddStoryPage> {
             ),
           
             //CATEGORY SELECTION
+            20.verticalSpace,
+            buildCategorySelection(),
 
             //TAG SELECTION
+            20.verticalSpace,
+            Text(Strings.tag, style: FontConstant.ratingPointDisplay,),
+            5.verticalSpace,
+            Wrap(
+              children: 
+                listTag.map((item) => _buildCustomSelection(item)).toList()),
+
+            40.verticalSpace,
           ],))
       ),
     );
-   }
+  }
+
+  Widget buildCategorySelection(){
+    if (isNonFiction) {
+      //get category
+    }
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Text(Strings.categories, style: FontConstant.ratingPointDisplay,),
+      5.verticalSpace,
+            GestureDetector(
+            onTap: () {
+              setState(() {
+                isCategorySelectExpand = ! isCategorySelectExpand;
+              });
+            },
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 16.sp, vertical: 5.sp),
+              decoration: const BoxDecoration(
+                border: Border(bottom: BorderSide(color: ColorConstants.lightText)),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    selectedItem ?? Strings.novelCategories,
+                    style: FontConstant.dropdownText.copyWith(fontWeight: FontWeight.bold),
+                  ),
+                  Icon(
+                    isCategorySelectExpand ? Icons.arrow_drop_up : Icons.arrow_drop_down,
+                    color: ColorConstants.buttonPastelGreen,
+                  ),
+                ],
+              ),
+            ),
+          ),
+           AnimatedSize(
+            duration: const Duration(milliseconds: 400),
+            curve: Curves.easeInOut,
+            child: isCategorySelectExpand
+                ? Container(
+                    margin: EdgeInsets.symmetric(horizontal: 16.sp),
+                    padding: EdgeInsets.only(top: 8.sp),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: ColorConstants.transparent),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: dropdownNovelCategories.map((String item) {
+                        return InkWell(
+                          onTap: () {},
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(vertical: 12.sp, horizontal: 16.sp),
+                            child: Text(item),
+                          ),
+                        );
+                      }).toList(),
+                    ),
+                  )
+                : 0.verticalSpace,
+          ),]);
+  }
+
+ 
+
+  Widget _buildCustomSelection(Category item) {
+    bool isSelected = selectedTags.contains(item.categoryId!);
+    return InkWell(
+      onTap: () => _toggleSelection(item.categoryId ?? -1),
+      borderRadius: BorderRadius.circular(25),
+      child: Container(
+        height: 40.sp,
+        margin: EdgeInsets.symmetric(vertical: 5.sp, horizontal: 10.sp),
+        padding: EdgeInsets.symmetric(vertical:  10.sp, horizontal: 15.sp),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20.sp),
+          color: isSelected ? ColorConstants.buttonLightGreen: ColorConstants.buttonPastelGreen,
+        ),
+        child: Wrap(
+          direction: Axis.horizontal,
+          children: [
+              Icon(
+              isSelected ? Icons.check : Icons.add,
+              color: ColorConstants.bgWhite,
+            ),
+            5.horizontalSpace,
+            Text(item.categoryName ?? '', style: FontConstant.chapterDisplay,),
+            3.horizontalSpace,
+        
+        ],)
+      ),
+    );
+  }
+
+   void _toggleSelection(int item) {
+    setState(() {
+      if (selectedTags.contains(item)) {
+        selectedTags.remove(item);
+      } else {
+        selectedTags.add(item);
+      }
+    });
+  }
 
   Future<void> _pickImage(ImageSource source) async {
     final pickedFile = await ImagePicker().pickImage(source: source);

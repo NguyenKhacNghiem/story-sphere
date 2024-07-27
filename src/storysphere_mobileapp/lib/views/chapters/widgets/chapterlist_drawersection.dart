b09fa7a293965468db23c9884543caf92491d14a
Nguyễn Quỳnh Như <ngquynhnhu1901@gmail.com>
2024-07-27 16:28:36 +0700

    service for chapter detail page

--- src/storysphere_mobileapp/lib/views/chapters/widgets/chapterlist_drawersection.dart
+++ src/storysphere_mobileapp/lib/views/chapters/widgets/chapterlist_drawersection.dart
@@ -5,7 +5,9 @@ import 'package:storysphere_mobileapp/constants/string.dart';
 import 'package:storysphere_mobileapp/constants/utils/color_constant.dart';
 import 'package:storysphere_mobileapp/constants/utils/font_constant.dart';
 import 'package:storysphere_mobileapp/models/chapter.dart';
+import 'package:storysphere_mobileapp/models/pagination_result.dart';
 import 'package:storysphere_mobileapp/routing/router.gr.dart';
+import 'package:storysphere_mobileapp/services/chapter_services.dart';
 
 class ChapterListDrawerSection extends StatefulWidget {
   final int storyId;
@@ -18,21 +20,21 @@ class ChapterListDrawerSection extends StatefulWidget {
 }
 
 class _ChapterListDrawerSection extends State<ChapterListDrawerSection> {
-  List<Chapter> chapterList = [
-    Chapter(chapterId: 1, chapterName: 'Cuộc gọi bí ẩn', chapterOrder: 1, chapterStatus: 1, viewCount: 1203, voteCount: 241, commentCount: 17, createdDate: DateTime(2023, 12, 20, 15, 30), fkStoryId: 1,
-            chapterContent: 'Trong căn phòng nhỏ, ánh đèn bàn mờ nhạt chiếu xuống những trang sách cũ kỹ. Minh Anh ngồi bên cửa lặng lẽ ngắm nhìn những chiếc lá rơi ngoài sân. Đêm nay, như mọi đêm khác, cô cảm thấy một nỗi buồn vô định xâm chiếm lòng mình. Bỗng, tiếng chuông điện thoại vang lên phá tan bầu không khí tĩnh mịch.'), 
-    Chapter(chapterId: 2, chapterName: 'Cánh cửa huyền bí', chapterOrder: 2, chapterStatus: 1, viewCount: 930, voteCount: 194, commentCount: 10, createdDate: DateTime(2024, 7, 20, 15, 30), fkStoryId: 1,
-            chapterContent: 'Trên đỉnh núi cao, mây trắng bao phủ, một ngôi đền cổ vẫn đứng vững giữa gió lạnh. Những câu chuyện lặng lẽ ngắm nhìn những chiếc lá rơi ngoài sân. Đêm nay, như mọi đêm khác, cô cảm thấy một nỗi buồn vô định xâm chiếm lòng mình. Bỗng, tiếng chuông điện thoại vang lên phá tan bầu không khí tĩnh mịch.'),   
-    Chapter(chapterId: 3, chapterName: 'Hành lang cổ kính', chapterOrder: 3, chapterStatus: 1, viewCount: 902, voteCount: 180, commentCount: 9,createdDate: DateTime(2024, 7, 20, 15, 30), fkStoryId: 1,
-            chapterContent: 'Trải dài bên trong lâu đài cổ kính, hành lang vắng lặng như giữ bí mật về quá khứ đầy bí ẩn của hoàng gia. lặng lẽ ngắm nhìn những chiếc lá rơi ngoài sân. Đêm nay, như mọi đêm khác, cô cảm thấy một nỗi buồn vô định xâm chiếm lòng mình. Bỗng, tiếng chuông điện thoại vang lên phá tan bầu không khí tĩnh mịch.'),   
-    Chapter(chapterId: 4, chapterName: 'Ánh sáng huyền dịu của Karith Thánh Mẫu', chapterOrder: 4, chapterStatus: 1, viewCount: 704, voteCount: 109, commentCount: 1,createdDate: DateTime(2024, 7, 20, 15, 30), fkStoryId: 1,
-            chapterContent: 'Trải dài bên trong lâu đài cổ kính, hành lang vắng lặng như giữ bí mật về quá khứ đầy bí ẩn của hoàng gia. lặng lẽ ngắm nhìn những chiếc lá rơi ngoài sân. Đêm nay, như mọi đêm khác, cô cảm thấy một nỗi buồn vô định xâm chiếm lòng mình. Bỗng, tiếng chuông điện thoại vang lên phá tan bầu không khí tĩnh mịch.'),   
-    Chapter(chapterId: 5, chapterName: 'Ánh sáng huyền dịu của Karith Thánh Mẫu', chapterOrder: 4, chapterStatus: 1, viewCount: 704, voteCount: 109, commentCount: 1,createdDate: DateTime(2024, 7, 20, 15, 30), fkStoryId: 1,
-            chapterContent: 'Trải dài bên trong lâu đài cổ kính, hành lang vắng'),   
-    Chapter(chapterId: 6, chapterName: 'Ánh sáng huyền dịu của Karith Thánh Mẫu', chapterOrder: 4, chapterStatus: 1, viewCount: 704, voteCount: 109, commentCount: 1,createdDate: DateTime(2024, 7, 20, 15, 30), fkStoryId: 1,
-            chapterContent: 'Trải dài bên trong lâu đài cổ kính, hành lang vắng lặng như giữ bí mật về quá khứ đầy bí ẩn của hoàng gia. Á...'),   
+  List<Chapter> chapterList = [];
+  //   Chapter(chapterId: 1, chapterName: 'Cuộc gọi bí ẩn', chapterOrder: 1, chapterStatus: 1, viewCount: 1203, voteCount: 241, commentCount: 17, createdDate: DateTime(2023, 12, 20, 15, 30), fkStoryId: 1,
+  //           chapterContent: 'Trong căn phòng nhỏ, ánh đèn bàn mờ nhạt chiếu xuống những trang sách cũ kỹ. Minh Anh ngồi bên cửa lặng lẽ ngắm nhìn những chiếc lá rơi ngoài sân. Đêm nay, như mọi đêm khác, cô cảm thấy một nỗi buồn vô định xâm chiếm lòng mình. Bỗng, tiếng chuông điện thoại vang lên phá tan bầu không khí tĩnh mịch.'), 
+  //   Chapter(chapterId: 2, chapterName: 'Cánh cửa huyền bí', chapterOrder: 2, chapterStatus: 1, viewCount: 930, voteCount: 194, commentCount: 10, createdDate: DateTime(2024, 7, 20, 15, 30), fkStoryId: 1,
+  //           chapterContent: 'Trên đỉnh núi cao, mây trắng bao phủ, một ngôi đền cổ vẫn đứng vững giữa gió lạnh. Những câu chuyện lặng lẽ ngắm nhìn những chiếc lá rơi ngoài sân. Đêm nay, như mọi đêm khác, cô cảm thấy một nỗi buồn vô định xâm chiếm lòng mình. Bỗng, tiếng chuông điện thoại vang lên phá tan bầu không khí tĩnh mịch.'),   
+  //   Chapter(chapterId: 3, chapterName: 'Hành lang cổ kính', chapterOrder: 3, chapterStatus: 1, viewCount: 902, voteCount: 180, commentCount: 9,createdDate: DateTime(2024, 7, 20, 15, 30), fkStoryId: 1,
+  //           chapterContent: 'Trải dài bên trong lâu đài cổ kính, hành lang vắng lặng như giữ bí mật về quá khứ đầy bí ẩn của hoàng gia. lặng lẽ ngắm nhìn những chiếc lá rơi ngoài sân. Đêm nay, như mọi đêm khác, cô cảm thấy một nỗi buồn vô định xâm chiếm lòng mình. Bỗng, tiếng chuông điện thoại vang lên phá tan bầu không khí tĩnh mịch.'),   
+  //   Chapter(chapterId: 4, chapterName: 'Ánh sáng huyền dịu của Karith Thánh Mẫu', chapterOrder: 4, chapterStatus: 1, viewCount: 704, voteCount: 109, commentCount: 1,createdDate: DateTime(2024, 7, 20, 15, 30), fkStoryId: 1,
+  //           chapterContent: 'Trải dài bên trong lâu đài cổ kính, hành lang vắng lặng như giữ bí mật về quá khứ đầy bí ẩn của hoàng gia. lặng lẽ ngắm nhìn những chiếc lá rơi ngoài sân. Đêm nay, như mọi đêm khác, cô cảm thấy một nỗi buồn vô định xâm chiếm lòng mình. Bỗng, tiếng chuông điện thoại vang lên phá tan bầu không khí tĩnh mịch.'),   
+  //   Chapter(chapterId: 5, chapterName: 'Ánh sáng huyền dịu của Karith Thánh Mẫu', chapterOrder: 4, chapterStatus: 1, viewCount: 704, voteCount: 109, commentCount: 1,createdDate: DateTime(2024, 7, 20, 15, 30), fkStoryId: 1,
+  //           chapterContent: 'Trải dài bên trong lâu đài cổ kính, hành lang vắng'),   
+  //   Chapter(chapterId: 6, chapterName: 'Ánh sáng huyền dịu của Karith Thánh Mẫu', chapterOrder: 4, chapterStatus: 1, viewCount: 704, voteCount: 109, commentCount: 1,createdDate: DateTime(2024, 7, 20, 15, 30), fkStoryId: 1,
+  //           chapterContent: 'Trải dài bên trong lâu đài cổ kính, hành lang vắng lặng như giữ bí mật về quá khứ đầy bí ẩn của hoàng gia. Á...'),   
     
-  ];
+  // ];
 
   @override
   void dispose() {
@@ -56,8 +58,10 @@ class _ChapterListDrawerSection extends State<ChapterListDrawerSection> {
               Strings.chapterList,
               style: FontConstant.bookTitleItem
             ),
-             
-            ListView.builder(
+            
+            chapterList.isEmpty
+            ? const CircularProgressIndicator()
+            : ListView.builder(
               scrollDirection: Axis.vertical,
                 controller: ScrollController(),
                 physics: const ClampingScrollPhysics(),
@@ -86,8 +90,20 @@ class _ChapterListDrawerSection extends State<ChapterListDrawerSection> {
 
   
   void initData() {
-    // Code ở đây sẽ chỉ chạy một lần khi state được khởi tạo
-    //data = widget.data;
+     // GET DATA
+     if (chapterList.isEmpty) {
+      final result =  ChapterService().getChaptesrByStoryId(widget.storyId, null);
+      result.whenComplete(() {
+        result.then((value) {
+          if (value != null && value is PaginationResult<Chapter> ) {
+            setState(() {
+              chapterList = value.result;
+            });
+          }
+        });
+      });
+    }
+    
     
 
   }

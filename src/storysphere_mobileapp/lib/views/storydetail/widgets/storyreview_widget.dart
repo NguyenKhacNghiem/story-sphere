ff450bef037717e85e03557ed07d9fe471746ded
Nguyễn Quỳnh Như <ngquynhnhu1901@gmail.com>
2024-07-28 11:42:31 +0700

    add reply review service

--- src/storysphere_mobileapp/lib/views/storydetail/widgets/storyreview_widget.dart
+++ src/storysphere_mobileapp/lib/views/storydetail/widgets/storyreview_widget.dart
@@ -23,12 +23,6 @@ class _StoryReviewWidget extends State<StoryReviewWidget> {
   List<Review> reviewList = [];
   int currentPage = 1;
   int totalPages = 1;
-  //   Review(reviewId: 1, userId: 1, storyId: 1, ratePoint: 5, reviewTime: DateTime(2021, 2, 18, 15, 30), reviewContent: 'Tác phẩm đã cuốn hút tôi ngay từ những trang đầu tiên. Bước vào cung điện của sự huyền bí, cùng với nhân vật Hue, tôi đã được đưa vào một thế giới đầy mê hoặc và kỳ diệu. Tác giả xây dựng cốt truyện chặt chẽ, mỗi chương đều đầy ắp những chi tiết ly kỳ và bất ngờ. Từ những thử thách cam go đến những bí mật cổ xưa, hành trình của Hue đã khiến tôi không thể đặt sách xuống. Một câu chuyện tuyệt vời, đầy sáng tạo và cảm xúc. Rất đáng để đọc!', replyTo: null ),
-  //   Review(reviewId: 2, userId: 2, storyId: 1, ratePoint: 1, reviewTime: DateTime(2021, 3, 20, 14, 21), reviewContent: 'Truyện không hay lắm', replyTo: null),
-  //   Review(reviewId: 3, userId: 3, storyId: 1, ratePoint: 1, reviewTime: DateTime(2022, 3, 12, 1, 30), reviewContent: 'Tác phẩm mở đầu rất hứa hẹn với một bối cảnh huyền bí và hấp dẫn. Hành trình của Hue bước vào cung điện cổ xưa đã khơi dậy sự tò mò và tưởng tượng. Tuy nhiên, mặc dù câu chuyện có nhiều chi tiết thú vị và đầy bất ngờ, nhưng một số chương lại hơi dàn trải và thiếu điểm nhấn. Một số đoạn cao trào chưa thực sự đủ sức gây ấn tượng mạnh mẽ.', replyTo: null),
-  //   Review(reviewId: 4, userId: 2, storyId: 1, ratePoint: null, reviewTime: DateTime(2022, 3, 12, 1, 30), reviewContent: 'Đọc tới chương 5 thì drop vì chịu không nổi nữ9', replyTo: 2),
-  //   Review(reviewId: 5, userId: 2, storyId: 1, ratePoint: null, reviewTime: DateTime(2022, 3, 12, 3, 30), reviewContent: 'Same, mình drop ở chương 10. Truyện này 3*', replyTo: 2),
-  // ];
  late Widget showMoreButton;
   bool isExpanded = false;
 
